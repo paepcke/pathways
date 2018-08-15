cb6f3fefbe67af6ce8d119dac007544539e71f77
Andreas Paepcke <paepcke@cs.stanford.edu>
2018-08-15 10:18:56 -0700

    Fuzzed with still partially implemented dendrogram.

--- src/pathways/course_dendrogram.py
+++ src/pathways/course_dendrogram.py
@@ -3,6 +3,9 @@ Created on Aug 13, 2018
 
 @author: paepcke
 '''
+from pathways.course_tsne_visualization import vector_creator
+from pathways.course_vector_creation import CourseVectorsCreator
+
 
 class Vec2DendrogramMaker(object):
     '''
@@ -10,8 +13,14 @@ class Vec2DendrogramMaker(object):
     '''
 
 
-    def __init__(self, params):
+    def __init__(self, course2vec_model_file):
         '''
         Constructor
         '''
+        vector_creator = CourseVectorsCreator()
+        course2vec_model = vector_creator.load_word2vec_model(course2vec_model_file)
         
+        
+if __name__ == '__main__':
+    Vec2DendrogramMaker('/users/Paepcke/Project/Pathways/Data/Course2VecData/course2vecModelWin10.model')
+            
