40a634546bd95e5826784404d8b9cc764dd4546b
Andreas Paepcke <paepcke@cs.stanford.edu>
2018-08-28 20:44:30 -0700

    Checkpoint

--- src/pathways/enrollment_plotter.py
+++ src/pathways/enrollment_plotter.py
@@ -5,6 +5,7 @@ Created on Aug 28, 2018
 '''
 import os
 import sqlite3
+import numpy as np
 
 import matplotlib.pyplot as plt
 
@@ -20,7 +21,8 @@ class EnrollmentPlotter(object):
         '''
         self.db_file = os.path.join(os.path.dirname(__file__), '../data/enrollment_tally.sqlite')
         self.per_quarter_enrollment_sums = self.get_sums(course_list)
-        print(self.per_quarter_enrollment_sums)
+        #****print(self.per_quarter_enrollment_sums)
+        self.plot_enrollment_history(self.per_quarter_enrollment_sums)
         
     def get_sums(self, course_list):
         conn = sqlite3.connect(self.db_file)
@@ -33,7 +35,22 @@ class EnrollmentPlotter(object):
         cur.execute(query)
         strm_sum_list = cur.fetchall()
         return strm_sum_list
+    
+    def plot_enrollment_history(self, data):
+        
+        quarters    = [int(strm_enrollment[0]) for strm_enrollment in data]
+        enrollments = [strm_enrollment[1] for strm_enrollment in data]
+        y_pos = np.arange(len(quarters))
+
+        plt.bar(y_pos, enrollments, align='center', alpha=0.5)
+        plt.xticks(y_pos, quarters)
+        plt.ylabel('Enrollment')
+        plt.title('Enrollment Since 2000')
+ 
+        plt.show()
 
 
 if __name__ == '__main__':
-    EnrollmentPlotter(['CS106A', 'CS106B'])        
+    #****EnrollmentPlotter(['CS106A', 'CS106B'])        
+    #EnrollmentPlotter(['CS106A'])        
+    EnrollmentPlotter(['MATH104'])
